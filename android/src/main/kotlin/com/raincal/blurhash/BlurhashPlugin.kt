4a547ff82fac037bd8a9c58856129b1fa14fe4f2
Raincal <cyj94228@gmail.com>
2021-01-20 16:06:54 +0800

    ♻️ Migrate to new plugins API

--- android/src/main/kotlin/com/raincal/blurhash/BlurhashPlugin.kt
+++ android/src/main/kotlin/com/raincal/blurhash/BlurhashPlugin.kt
@@ -3,22 +3,29 @@ package com.raincal.blurhash
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import java.io.ByteArrayOutputStream
+
+import androidx.annotation.NonNull
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class BlurhashPlugin: MethodCallHandler {
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "blurhash")
-      channel.setMethodCallHandler(BlurhashPlugin())
-    }
+class BlurhashPlugin: FlutterPlugin, MethodCallHandler {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private lateinit var channel : MethodChannel
+
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "blurhash")
+    channel.setMethodCallHandler(this)
   }
 
-  override fun onMethodCall(call: MethodCall, result: Result) {
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     if (call.method == "blurHashEncode") {
       val image = call.argument<ByteArray>("image")!!
       val componentX = call.argument<Int>("componentX")!!
@@ -50,5 +57,9 @@ class BlurhashPlugin: MethodCallHandler {
     } else {
       result.notImplemented()
     }
+  }
+
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
   }
 }
