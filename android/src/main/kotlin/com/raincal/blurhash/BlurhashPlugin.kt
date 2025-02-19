91060ac26499f7aa19975d69554c4fa267ec5894
Raincal <cyj94228@gmail.com>
2025-02-19 18:14:09 +0800

    fix build error on Flutter 3.29.0

--- android/src/main/kotlin/com/raincal/blurhash/BlurhashPlugin.kt
+++ android/src/main/kotlin/com/raincal/blurhash/BlurhashPlugin.kt
@@ -11,7 +11,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 class BlurhashPlugin: FlutterPlugin, MethodCallHandler {
   /// The MethodChannel that will the communication between Flutter and native Android
